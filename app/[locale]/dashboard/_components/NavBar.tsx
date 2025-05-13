82123b307dab8643092d266c3b1e75bdca0426c5
ahmadaalabbas <ahmadalabbas2002@gmail.com>
2025-05-13 19:21:27 +0300

    added dashboard navbar

---
+++ app/[locale]/dashboard/_components/NavBar.tsx
@@ -0,0 +1,30 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "@/i18n/routing";
+import { useTranslations } from "next-intl";
+
+function NavBar() {
+  const t = useTranslations("dashboard");
+  const navTexts = [
+    {
+      text: t("buttonTextOne"),
+      url: "/dashboard",
+    },
+    {
+      text: t("buttonTextTwo"),
+      url: "/dashboard/orders",
+    },
+  ];
+  return (
+    <nav className="w-full flex gap-x-5 justify-center items-center">
+      {navTexts.map((button, index) => (
+        <Button key={index} asChild className="py-2 px-5  font-semibold">
+          <Link href={button.url} className="px-12">
+            {button.text}
+          </Link>
+        </Button>
+      ))}
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ app/[locale]/dashboard/orders/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      orders page
+    </div>
+  )
+}
+
+export default page
